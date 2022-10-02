587237d99ea724e847664dfc4c90c01e4fd29dad
Santhoshprathik <92070694+Santhoshprathik@users.noreply.github.com>
2022-10-02 19:00:52 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,16 +1,11 @@
 <?php
-$HOSTNAME='sql212.epizy.com';
-$USERNAME='epiz_32710230';
-$PASSWORD='psfgJ13fZQK';
+$HOSTNAME='********';
+$USERNAME='************';
+$PASSWORD='************';
 $DATABASE='epiz_32710230_usersdetails';
 
 $con=mysqli_connect($HOSTNAME,$USERNAME,$PASSWORD,$DATABASE);
 
-$sql="CREATE TABLE `payment_app`.`usertable` (`id` INT(255) NOT NULL AUTO_INCREMENT ,
- `email` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`),
-  UNIQUE (`email`))"
-
-$result=mysqli_query($con,$sql);
 if(!$con){
     die(mysqli_error($con));
 }
